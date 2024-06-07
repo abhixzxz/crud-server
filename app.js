d07a8957f18801a74bc356c112ce4bb1de7f7c18
Abiraj k <abhi@Abirajs-MacBook-Air.local>
2024-06-07 11:32:36 +0530

    added deployment changes

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ require("dotenv").config();
 
 const corsOptions = {
   origin: ["http://localhost:3000", "https://crud-client-rho.vercel.app"],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
 };
 
@@ -16,24 +18,26 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
-// Debugging middleware
-app.use((req, res, next) => {
-  console.log(`Request Method: ${req.method}, Request URL: ${req.url}`);
-  console.log(`Request Headers:`, req.headers);
-  next();
-});
-
 // Middleware
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions)); // Handle preflight requests
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use("/storage", express.static(__dirname + "/storage"));
+
+// CORS preflight handling
+app.options("*", (req, res) => {
+  res.header("Access-Control-Allow-Origin", req.headers.origin);
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.sendStatus(200);
+});
 
 // Routes section
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
 app.use("/api/auth", authRoutes);
 app.use("/api/companies", companiesRoute);
 app.use("/api/employees", employeesRoute);
